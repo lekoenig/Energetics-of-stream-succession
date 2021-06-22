fc154014b30e5261594fd3973be232705110f754
Lauren Koenig <lrnkoenig@gmail.com>
2021-06-22 16:47:11 -0400

    Download model fits and export for use in modeling scripts

--- 1_Filter_sites.R
+++ 1_Filter_sites.R
@@ -28,10 +28,10 @@ out_save_dir <- "./data/out"
 # Read in NWIS sites from Powell Center data set (Appling et al. 2018):
 pc_sites <- download_site_data(in_save_dir)
 
-# Read in Powell Center data (metabolism estimates and predictors):
+# Read in Powell Center data (metabolism estimates and predictors) - exports file called 'daily_predictions.csv':
 metab_all <- download_metab_est(in_save_dir) 
 
-# Read in Powell Center data (model diagnostics):
+# Read in Powell Center data (model diagnostics) - exports file called 'diagnostics.csv':
 diagnostics <- download_model_diagnostics(in_save_dir)
 
 ##===================================================================##
@@ -65,8 +65,8 @@ metab_filter <- metab_all %>%
                   filter(GPP_grow_median > 3) 
                   
 # Inspect filtered correlation coefficients and growing season GPP:
-hist(unique(metab_filter$cor.coef))
-hist(unique(metab_filter$GPP_grow_median))
+quantile(unique(metab_filter$cor.coef))
+quantile(unique(metab_filter$GPP_grow_median))
 
 # Find out whether sites are co-located with turbidity sensors:
 metab_filtered_sites <- bind_rows(lapply(unique(metab_filter$site_name),check_turbidity_data)) %>%
@@ -77,7 +77,7 @@ metab_filtered_sites <- bind_rows(lapply(unique(metab_filter$site_name),check_tu
                         # check whether the metabolism and turbidity date ranges overlap:
                         mutate(check_turbidity_range = int_overlaps(turb_interval,metab_interval)) %>%
                         # filter sites that overlap with turbidity data (at least 180 days):
-                        filter(.,turbidity_total_days > 80 & check_turbidity_range == "TRUE") %>% 
+                        filter(.,turbidity_total_days > 180 & check_turbidity_range == "TRUE") %>% 
                         # bind with site info from Science Base and select columns:
                         left_join(.,pc_sites,by="site_name") %>%
                         select(site_name,long_name,nhdplus_id,drain_area_km2,lat,lon,coord_datum,alt,alt_datum,site_type,dvqcoefs.c,dvqcoefs.f,
@@ -95,10 +95,16 @@ write.csv(metab_filtered_sites,paste(out_save_dir,"/site_data_filtered.csv",sep=
 metab_filtered_preds <- filter(metab_all,metab_all$site_name %in% metab_filtered_sites$site_name)
 write.csv(metab_filtered_preds,paste(out_save_dir,"/daily_predictions_filtered.csv",sep=""),row.names = FALSE)
 
+##===================================================================##
+##              Export model fits for filtered sites                 ##
+##===================================================================##
+    
+# Read in Powell Center data (model fits):
+fit_ids <- get_modfit_ids()
+fit_ids_sites_filtered <- fit_ids %>% filter(site_name %in% metab_filtered_sites$site_name)
+fits <- lapply(fit_ids_sites_filtered[,"file_id"],download_model_fits)
 
-    
-    
-    
-    
-    
+# Export fits:
+names(fits) <- fit_ids_sites_filtered$site_name
+saveRDS(fits,"./data/out/model_fits_filtered.rds")
     
